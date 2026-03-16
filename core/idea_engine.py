e37781f44c83b21a455ea515a52786b75abd6fe7
ordinaria_tech <mikaeleneres1@gmail.com>
2026-03-16 14:27:56 -0300

    Create idea_engine.py

---
+++ core/idea_engine.py
@@ -0,0 +1,12 @@
+import random
+
+ideas = [
+    "mistérios do universo",
+    "psicologia humana",
+    "tecnologia do futuro",
+    "curiosidades científicas",
+    "segredos da mente humana"
+]
+
+def get_idea():
+    return random.choice(ideas)
